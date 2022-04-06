980cb2ed01ca9a0aa606ee02e15194f72c03e804
naamagi <n.gilat@gmail.com>
2022-04-06 23:52:59 +0300

    first commit

---
+++ function-declaration-part-2.js
@@ -0,0 +1,52 @@
+/*
+    JavaScript - Declaring Functions
+
+    The following exercise contains the following subjects:
+        * functions
+    
+    Instructions
+        * Please reformat the following function expressions to IIFE functions.
+        * Please reformat the following function declarations in two ways, explicit return and implicit return functions.
+
+    Submit the file to Hive
+
+*/
+
+// From function declarations to explicit and implicit return functions (one of each).
+// function welcome() {
+//     let welcome = 'Welcome to Appleseeds Bootcamp!';
+//     return welcome;
+// }
+// explicit return:
+const welcomeMessage1 = () => {
+  let welcome = "Welcome to Appleseeds Bootcamp!";
+  return welcome;
+};
+// implicit return:
+const welcomeMessage2 = () => "Welcome to Appleseeds Bootcamp!";
+
+
+// function power(a) {
+//     let myNumber = a;
+//     let result = Math.pow(myNumber, 2);
+//     return result;
+// }
+// explicit return:
+const power1 = (a) => {
+  let myNumber = a;
+  let result = Math.pow(myNumber, 2);
+  return result;
+};
+// implicit return:
+const power2 = (a) => Math.pow(a, 2);
+
+
+
+// From function expressions to IIFE functions.
+
+// const squareRoot = (a) => Math.sqrt(a);
+const squareRoot = ((a) => Math.sqrt(a))();
+
+
+// const randomNumbers = (a, b) => Math.random() * (a - b) + b;
+const randomNumbers = ((a, b) => Math.random() * (a - b) + b)();
